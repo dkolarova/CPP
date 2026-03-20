dc73354b85f50d8fd36b8f7d554f3c18bb848882
Diana Kolarova <136117521+dkolarova@users.noreply.github.com>
2026-03-20 16:15:50 +0100

    adding starting files for cpp04

---
+++ module_04/ex00/include/Animal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkolarov <dkolarov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/20 16:01:32 by dkolarov          #+#    #+#             */
+/*   Updated: 2026/03/20 16:10:33 by dkolarov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal 
+{
+	protected:
+		std::string type;
+
+	public:
+		Animal();
+		Animal(const Animal& other);
+		Animal& operator=(const Animal& other);
+	
+		virtual ~Animal();
+
+		virtual void makeSound() const;
+		std::string getType() const;
+};
+
+#endif
